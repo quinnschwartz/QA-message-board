a9b953b815af68942703d726a1e26953c69b7274
quinnschwartz <quinnschwartz@gmail.com>
2017-01-13 13:21:36 -0800

    add new answer component that enables the action of saving a new answer to the message board.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question'),
+      answers: this.store.findAll('answer')
+    });
   },
 
   actions: {
@@ -11,7 +14,11 @@ export default Ember.Route.extend({
       newQuestion.save();
       this.transitionTo('index');
     },
-
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
+      this.transitionTo('index');
+    },
     destroyQuestion(question) {
       question.destroyRecord();
       this.transitionTo('index');
