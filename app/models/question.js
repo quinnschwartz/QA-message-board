3a16457f0168ae896980c3adb4336b3dbd0f7645
quinnschwartz <quinnschwartz@gmail.com>
2017-01-13 01:23:49 -0800

    add new question component that enables the ability to create and save a new question to the data base and display it in the template.

--- app/models/question.js
+++ app/models/question.js
@@ -2,4 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   content: DS.attr();
+  author: DS.attr();
+  notes: DS.attr();
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findALL('question');
+  },
+
+  actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+
+    destroyRental(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
